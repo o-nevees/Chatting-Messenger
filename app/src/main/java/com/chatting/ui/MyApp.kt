e0c82e70105a98e4905db6aed13f673eba58a20d
o-nevees <nevesleandroribeiro@gmail.com>
2025-11-06 07:08:08 -0300

    atualização correçõesde bugsemenhorias

--- app/src/main/java/com/chatting/ui/MyApp.kt
+++ app/src/main/java/com/chatting/ui/MyApp.kt
@@ -3,9 +3,6 @@ package com.chatting.ui
 import android.app.Application
 import android.content.Context
 import android.util.Log
-import androidx.lifecycle.DefaultLifecycleObserver
-import androidx.lifecycle.LifecycleOwner
-import androidx.lifecycle.ProcessLifecycleOwner
 import com.chatting.domain.AccountManager
 import com.chatting.domain.MediaManager
 import com.chatting.domain.MessageManager
@@ -52,30 +49,6 @@ class MyApp : Application() {
         }
     }
 
-    private val appLifecycleObserver = object : DefaultLifecycleObserver {
-        override fun onStart(owner: LifecycleOwner) {
-            if (authRepository.authStateFlow.value == AuthState.LOGGED_IN) {
-                Log.d(TAG, "App in foreground AND Logged In. Attempting WebSocket connection...")
-                try {
-                    webSocketClient.connect()
-                } catch (e: Exception) {
-                    Log.e(TAG, "Error connecting WebSocket onStart", e)
-                }
-            } else {
-                Log.d(TAG, "App in foreground but Logged Out. Skipping WebSocket connection.")
-            }
-        }
-
-        override fun onStop(owner: LifecycleOwner) {
-            Log.d(TAG, "App in background. Disconnecting WebSocket.")
-            try {
-                webSocketClient.disconnect()
-            } catch (e: Exception) {
-                Log.e(TAG, "Error disconnecting WebSocket onStop", e)
-            }
-        }
-    }
-
     override fun onCreate() {
         super.onCreate()
         Log.d(TAG, "onCreate started.")
@@ -96,9 +69,6 @@ class MyApp : Application() {
 
         webSocketMessageHandler.startListening()
         Log.d(TAG, "WebSocketMessageHandler listener started.")
-
-        ProcessLifecycleOwner.get().lifecycle.addObserver(appLifecycleObserver)
-        Log.d(TAG, "LifecycleObserver added.")
 
         getAndStoreFCMToken()
         Log.d(TAG, "onCreate finished.")
